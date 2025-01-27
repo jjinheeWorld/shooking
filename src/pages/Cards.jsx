8e45fa876d2df812ebe7cc67b2c0bdf1c0d66ef6
jjinheeWorld <wlsgml450@naver.com>
2025-01-27 17:34:57 +0900

    style: AddCardButton 컴포넌트 UI 구현하기

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,9 +1,16 @@
 import SubHeader from "../components/SubHeader";
+import AddCardButton from "../components/AddCardButton";
 
 const Cards = () => {
   return (
     <div>
       <SubHeader title={"보유카드"} />
+      <div className="mt-[69px] flex flex-col items-center">
+        <div className="pt-[17px] pb-[9px] text-[#575757] font-bold text-sm">
+          새로운 카드를 등록해주세요.
+        </div>
+        <AddCardButton />
+      </div>
     </div>
   );
 };
