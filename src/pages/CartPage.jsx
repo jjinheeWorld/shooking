a4dd81a7b23613af5761a56c0f8e001b27b729f8
jjinheeWorld <wlsgml450@naver.com>
2025-02-16 23:52:25 +0900

    style: CartSummary 컴포넌트 UI 구현하기

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,11 +1,13 @@
 import MainHeader from "../components/MainHeader";
 import CartList from "../components/\bCartList";
+import CartSummary from "../components/CartSummary";
 
 const CartPage = () => {
   return (
     <div>
       <MainHeader showBackBtn={true} showCartIcon={false} />
       <CartList />
+      <CartSummary />
     </div>
   );
 };
